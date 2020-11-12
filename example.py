1f665cabda20200451c4aab7a40bcfa193a9adde
Jimmy Prior <68312191+jimmyprior@users.noreply.github.com>
2020-11-12 09:15:42 -0500

    Added some comments

--- example.py
+++ example.py
@@ -1,30 +1,23 @@
 from PIL import Image
 from brickify import EditedImage, PieceList, RenderedImage
-from defaults.palettes import all
-from defaults.pieces import medium
+from defaults.palettes import all   #use the default lego color palette 
+from defaults.pieces import medium #use medium piece sizes 
 
-
-
-im = Image.open('output/barack.jpg')
-
-new_im = im.resize((2000, 2000))
-
-new_im.save('for_imgur.jpg')
-
-exit()
 
 IMAGE = 'barack.jpg'
 INPUT_PATH = 'input/'
 OUTPUT_PATH = 'output/'
 
-SIZE = (96, 96)
+SIZE = (96, 96)   # each pixel will be treated as a (1x1) lego brick
 
-im = Image.open(INPUT_PATH + IMAGE)
+im = Image.open(INPUT_PATH + IMAGE)   # open the image
 
+edited = EditedImage(image = im).edit_image(size = SIZE, palette = all) #resize the image and recolor it to the lego colors
+pieces = PieceList(image = edited).generate_medium(pieces = medium)  #use medium sized pieces and generate with medium speed. 
+#Medium speed is not optimized to fit the biggest piece when possible. 
+#use the generate_slow method if you're looking for optimization.
+#use the generate_fast method if you're looking for speed.  
 
-edited = EditedImage(image = im).edit_image(size = SIZE, palette = all)
-pieces = PieceList(image = edited).generate_medium(pieces = medium)
-render = RenderedImage(piece_list = pieces, size = SIZE).render_image()
+render = RenderedImage(piece_list = pieces, size = SIZE).render_image() #render the piecelist into a visual form. 
 
-
-render.save(OUTPUT_PATH + IMAGE)
+render.save(OUTPUT_PATH + IMAGE) #save the render you created. 
